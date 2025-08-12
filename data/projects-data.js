c4448cf2dac3e005abfdd1a8880dc401ebba88a9
Mrs Crypt <reokaro89@gmail.com>
2025-08-12 15:13:30 +0800

    Update projects-data.js

--- data/projects-data.js
+++ data/projects-data.js
@@ -1,15 +1,21 @@
 export const projectsData = [
   {
-    image: 'cat-profile-2.png', // Add your project image to the "images-and-icons/images" folder, then specify the filename here.
-    title: 'Project-Title 1',
-    detail: 'Lorem ipsum dolor, sit amet consectetur.',
-    link: 'https://cryptologism.github.io/arachnidsportfolio/page.html', //Host or upload your project in internet then paste the link here
+    image: 'https://static.wixstatic.com/media/52b1a3_1d72fe1a7b894ce0880f10dd6a0a5c3b~mv2.png/v1/fill/w_128,h_128,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Gimmedia-logo-w-v.png',
+    title: 'Gimmedia Kelantan',
+    detail: 'Regional Gimmedia hub for Kelantan, featuring livestream schedules, local event updates, and content tailored to the state’s audience.',
+    link: 'https://cryptologism.github.io/gimmediakelantan/',
   },
   {
-    image: 'cat-profile-2.png', // Add your project image to the "images-and-icons/images" folder, then specify the filename here.
-    title: 'Project-Title 2',
-    detail: 'Lorem ipsum dolor, sit amet consectetur.',
-    link: 'https://cryptologism.github.io/arachnidsportfolio/page.html' //Host or upload your project in internet then paste the link here
+    image: 'https://cdn.jsdelivr.net/gh/Cryptologism/gg@main/img/arachnids2.png',
+    title: 'The Arachnids Blog',
+    detail: 'A personal blog blending creative writing, commentary, and niche insights across culture, media, and everyday life.',
+    link: 'https://thearachnids.blogspot.com/',
   },
-  // Add more projects if needed 
-]
+  {
+    image: 'https://platinumhotelcafe.com/wp-content/uploads/2024/07/PlatinumHotel-150x150.jpeg',
+    title: 'Platinum Hotel Café',
+    detail: 'Showcase site for Platinum Hotel Café, highlighting its signature menu, dining experience, and special event offerings.',
+    link: 'https://platinumhotelcafe.com/',
+  },
+  // Add more projects if needed
+];
