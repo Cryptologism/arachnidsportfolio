2fc5e82b43905c52a1cf5f2522c1eab4559bc632
Mrs Crypt <reokaro89@gmail.com>
2025-08-12 10:24:09 +0800

    Update personal-data.js

--- data/personal-data.js
+++ data/personal-data.js
@@ -2,19 +2,19 @@ export const personalData = {
   // Personal Information
   name: '🕷️ The-Arachnids',
   mainSkill: 'Web Developer',
-  school: 'SMK TLDM 1 / IKBN Seri Iskandar (Major Networking)',
-  address: 'Pasir Puteh, Kelantan',
-  contactNumber: '+60 169656877',
+  school: 'National Youth Skills Institute, Seri Iskandar (Computer Networking)',
+  address: 'Pasir Puteh, Kelantan, Malaysia',
+  contactNumber: '+60 16-965 6877',
   email: 'reokaro89@gmail.com',
 
   // Resume and Profile
-  resume: 'meowish-resume.pdf', // Add your PDF resume to the main folder and specify its filename here
-  profilePicture: 'cat-profile-2.png', // Add your profilePicture to the  "images-and-icons/images" folder and specify its filename here
+  resume: 'meowish-resume.pdf', // Place your PDF resume in the main folder
+  profilePicture: 'cat-profile-2.png', // Place your profile picture in the "images-and-icons/images" folder
 
   // EmailJS Configuration
-  publicKey: 'YourPublicKey', // Go to emailJS.com, sign up, and get your public-key, then paste here
-  serviceId: 'service_id', // Go to emailJS.com, sign up, and get your service-id then, paste here. 
-  templateId: 'template_id',  // Go to emailJS.com, sign up, and get your template-id then, paste here. 
+  publicKey: 'YourPublicKey', // Get from EmailJS.com
+  serviceId: 'service_id', // Get from EmailJS.com
+  templateId: 'template_id', // Get from EmailJS.com
 
   // Social Media Links
   facebookLink: 'https://www.facebook.com/CCGamingOfficialPage/',
@@ -24,5 +24,3 @@ export const personalData = {
   // Work Status
   workAvailabilityStatus: 'Available for work',
 };
-
-
