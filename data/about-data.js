3553c204766d1c8eee8979602348634d9d07b1f5
Mrs Crypt <reokaro89@gmail.com>
2025-08-12 10:24:53 +0800

    Update about-data.js

--- data/about-data.js
+++ data/about-data.js
@@ -5,19 +5,19 @@ I believe great systems don’t just work—they think, adapt, and evolve.`,
   
   certificates: [
     {
-      name: 'Royal Malaysian Navy 1 National Primary School – Armed Forces Academic Institution',
+      name: 'Royal Malaysian Navy 1 National Primary School\nArmed Forces Academic Institution',
       year: 'Graduated: 2001',
     },
     {
-      name: 'Royal Malaysian Navy 1 National Secondary School – Armed Forces Academic Institution',
+      name: 'Royal Malaysian Navy 1 National Secondary School\nArmed Forces Academic Institution',
       year: 'Graduated: 2006',
     },
     {
-      name: 'Ahmad Boestamam National Secondary School – Form 6 (Lower & Upper Six), Arts Stream, Major in History & Geography',
+      name: 'Ahmad Boestamam National Secondary School – Form 6 (Lower & Upper Six)\nArts Stream, Major in History & Geography',
       year: 'Attended: 2007 – Graduated: 2008',
     },
     {
-      name: 'National Youth Skills Institute, Seri Iskandar – Major in Computer Network Technician',
+      name: 'National Youth Skills Institute, Seri Iskandar\nMajor in Computer Network Technician',
       year: 'Attended: 2013 – Graduated: 2015',
     },
   ],
