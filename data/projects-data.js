f51b35df5f9d7bbf4790e4071e4eda2dd8d56011
Mrs Crypt <reokaro89@gmail.com>
2025-08-12 16:08:09 +0800

    Update projects-data.js

--- data/projects-data.js
+++ data/projects-data.js
@@ -1,21 +1,22 @@
 export const projectsData = [
   {
-    image: 'https://static.wixstatic.com/media/52b1a3_1d72fe1a7b894ce0880f10dd6a0a5c3b~mv2.png/v1/fill/w_128,h_128,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Gimmedia-logo-w-v.png',
+    image: 'Gimmedia.png',
     title: 'Gimmedia Kelantan',
     detail: 'Regional Gimmedia hub for Kelantan, featuring livestream schedules, local event updates, and content tailored to the state’s audience.',
     link: 'https://cryptologism.github.io/gimmediakelantan/',
   },
   {
-    image: 'https://cdn.jsdelivr.net/gh/Cryptologism/gg@main/img/arachnids2.png',
+    image: 'Arachnids.png',
     title: 'The Arachnids Blog',
     detail: 'A personal blog blending creative writing, commentary, and niche insights across culture, media, and everyday life.',
     link: 'https://thearachnids.blogspot.com/',
   },
   {
-    image: 'https://platinumhotelcafe.com/wp-content/uploads/2024/07/PlatinumHotel-150x150.jpeg',
+    image: 'PlatinumHotel.jpeg',
     title: 'Platinum Hotel Café',
     detail: 'Showcase site for Platinum Hotel Café, highlighting its signature menu, dining experience, and special event offerings.',
     link: 'https://platinumhotelcafe.com/',
   },
   // Add more projects if needed
 ];
+
