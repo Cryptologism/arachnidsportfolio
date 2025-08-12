8ecf926dcb1f18f98ea03c70e609cfac28e4c45a
Mrs Crypt <reokaro89@gmail.com>
2025-08-12 10:33:21 +0800

    Update personal-data.js

--- data/personal-data.js
+++ data/personal-data.js
@@ -1,10 +1,18 @@
 export const personalData = {
   // Personal Information
-  name: '🕷️ The-Arachnids',
-  mainSkill: 'Web Developer',
-  school: 'National Youth Skills Institute, Seri Iskandar (Computer Networking)',
-  address: 'Pasir Puteh, Kelantan, Malaysia',
-  contactNumber: '+60 16-965 6877',
+  name: '🕷️ The Arachnids',
+  professionalTitle: 'Web Developer & Computer Systems Technician',
+  coreSkills: [
+    'Web Development',
+    'Computer Hardware & Software Troubleshooting',
+    'Networking & System Administration',
+    'IT Support & Maintenance',
+    'Custom PC Assembly & Repair',
+    'Cybersecurity Basics'
+  ],
+  education: 'National Youth Skills Institute, Seri Iskandar – Certified Computer Network Technician',
+  location: 'Pasir Puteh, Kelantan, Malaysia',
+  phone: '+60 16-965 6877',
   email: 'reokaro89@gmail.com',
 
   // Resume and Profile
@@ -22,5 +30,5 @@ export const personalData = {
   tiktokLink: 'https://www.tiktok.com/@sieureastofficial',
 
   // Work Status
-  workAvailabilityStatus: 'Available for work',
+  workAvailabilityStatus: 'Available for work'
 };
