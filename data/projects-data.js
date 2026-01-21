460e2e24a7e4126c50517bdb103c686b3a26ba12
Sir-Crypto a.k.a The-Arachnids <reokaro89@gmail.com>
2026-01-21 22:46:55 +0800

    Update projects-data.js

--- data/projects-data.js
+++ data/projects-data.js
@@ -17,14 +17,5 @@ export const projectsData = [
     detail: 'A personal blog blending creative writing, commentary, and niche insights across culture, media, and everyday life.',
     link: 'https://thearachnids.blogspot.com/',
   },
-  {
-  image: 'GitHub.jpg',
-  title: 'My GitHub Portfolio',
-  detail: 'Explore my GitHub repositories, open-source projects, and contributions. Includes coding experiments, automation tools, and portfolio work.',
-  link: 'https://github.com/Cryptologism',
-},
   // Add more projects if needed
 ];
-
-
-
