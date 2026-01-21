9bd47b4016bfbd4000612825cc3d76fe4c545f6c
Sir-Crypto a.k.a The-Arachnids <reokaro89@gmail.com>
2026-01-21 22:49:06 +0800

    Update projects-data.js

--- data/projects-data.js
+++ data/projects-data.js
@@ -1,21 +1,15 @@
 export const projectsData = [
   {
-    image: 'Gimmedia.png',
-    title: 'Gimmedia Kelantan',
-    detail: 'Regional Gimmedia hub for Kelantan, featuring livestream schedules, local event updates, and content tailored to the state’s audience.',
-    link: 'https://cryptologism.github.io/gimmediakelantan/',
+    image: 'cat-profile-2.png', // Add your project image to the "images-and-icons/images" folder, then specify the filename here.
+    title: 'Project-Title 1',
+    detail: 'Lorem ipsum dolor, sit amet consectetur.',
+    link: 'https://cryptologism.github.io/arachnidsportfolio/page.html', //Host or upload your project in internet then paste the link here
   },
   {
-    image: 'PlatinumHotel.jpeg',
-    title: 'Platinum Hotel Café',
-    detail: 'Showcase site for Platinum Hotel Café, highlighting its signature menu, dining experience, and special event offerings.',
-    link: 'https://platinumhotelcafe.com/',
+    image: 'cat-profile-2.png', // Add your project image to the "images-and-icons/images" folder, then specify the filename here.
+    title: 'Project-Title 2',
+    detail: 'Lorem ipsum dolor, sit amet consectetur.',
+    link: 'https://cryptologism.github.io/arachnidsportfolio/page.html' //Host or upload your project in internet then paste the link here
   },
-  {
-    image: 'Arachnids.png',
-    title: 'The Arachnids Blog',
-    detail: 'A personal blog blending creative writing, commentary, and niche insights across culture, media, and everyday life.',
-    link: 'https://thearachnids.blogspot.com/',
-  },
-  // Add more projects if needed
-];
+  // Add more projects if needed 
+]
